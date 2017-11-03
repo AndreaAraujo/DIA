3240a1cf7487e6fa798ba1d23aa6d97580c7de4d
Crmiguez <crmiguez@esei.uvigo.es>
2017-11-03 14:49:40 +0100

    Modelo clases adaptado al informe anual
    Idea inicial de las clases a usar por el gráfico.

---
+++ crmiguez/GraficoAnualNatacion/GraficoAnualNatacion/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GraficoAnualNatacion
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+		}
+	}
+}
---
+++ crmiguez/GraficoAnualNatacion/GraficoAnualNatacion/Properties/Medidas.cs
@@ -0,0 +1,44 @@
+﻿using System;
+namespace GraficoAnualNatacion
+{
+	public class Medidas
+	{
+		public Medidas(double pes, double circle, String notsMeds)
+		{
+			this.Peso = pes;
+			this.Circunferencia = circle;
+			this.NotasMedidas = notsMeds;
+		}
+
+		public double Peso {
+			get {
+				return this.peso;
+			}
+			set {
+				this.peso = value;
+			}
+		}
+
+		public double Circunferencia {
+			get {
+				return this.circunferencia;
+			}
+			set {
+				this.circunferencia = value;
+			}
+		}
+
+		public String NotasMedidas {
+			get {
+				return this.notasMedidas;
+			}
+			set {
+				this.notasMedidas = value;
+			}
+		}
+
+		private double peso;
+		private double circunferencia;
+		private String notasMedidas;
+	}
+}
